61ca2c2963f39eb816c99e132feb8b9dafe06e59
Meor Izzuddin <meorizzuddin@hopexito.com>
2023-03-15 22:58:23 +0800

    Admin Storage 2.0
    God Mode Opened for Storage Optimization

--- app/Http/Livewire/Admin/AdminStorage.php
+++ app/Http/Livewire/Admin/AdminStorage.php
@@ -2,14 +2,42 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Artist;
+use App\Models\Product;
+use App\Models\ProductCollection;
+use App\Models\ProductTemplate;
 use App\Models\TemporaryFile;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class AdminStorage extends Component
 {
+
+    private function formatBytes($bytes, $precision = 2){
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
     public function render()
-    {   
+    {
+        $diskSize = collect(Storage::disk('public')->allFiles())->map(function ($path) {
+            return Storage::disk('public')->size($path);
+        })->sum();
+        $formattedDisk = $this->formatBytes($diskSize);
+
+        $collection_image_path = ProductCollection::pluck('collection_image');
+        $cover_image_path = Artist::whereNotNull('cover_image')->pluck('cover_image');
+        $image_back_path = Product::whereNotNull('image_back')->pluck('image_back');
+        $image_front_path = Product::whereNotNull('image_front')->pluck('image_front');
+        $mockup_image_path = ProductTemplate::whereNotNull('mockup_image')->pluck('mockup_image');
+        $profile_photos_path = User::whereNotNull('profile_photo_path')->pluck('profile_photo_path');
         $temp = TemporaryFile::get();
         $collection_image = Storage::files('public/collection-image');
         $collection_image_files = array_map('basename', $collection_image);
@@ -24,6 +52,6 @@ class AdminStorage extends Component
         $profile_photos = Storage::files('public/profile-photos');
         $profile_photos_files = array_map('basename', $profile_photos);
 
-        return view('livewire.admin.admin-storage', compact('temp','collection_image_files','cover_image_files','image_back_files','image_front_files','mockup_image_files','profile_photos_files'));
+        return view('livewire.admin.admin-storage', compact('formattedDisk','collection_image_path', 'cover_image_path', 'image_back_path', 'image_front_path', 'mockup_image_path', 'profile_photos_path', 'temp', 'collection_image_files', 'cover_image_files', 'image_back_files', 'image_front_files', 'mockup_image_files', 'profile_photos_files'));
     }
 }
--- resources/views/livewire/admin/admin-storage.blade.php
+++ resources/views/livewire/admin/admin-storage.blade.php
@@ -2,8 +2,17 @@
     <x-jet-admin-sidebar />
     <x-jet-admin-layout>
         <x-jet-admin-card>
-            <x-jet-admin-header>Temporary Files</x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-6">
+            <x-jet-admin-header>Storage Disk 
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 mt-6">
+                {{ $formattedDisk }}
+            </div>
+        </x-jet-admin-card>
+        <x-jet-section-border/>
+        <x-jet-admin-card>
+            <x-jet-admin-header>Temporary Files <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($temp) }}</span></x-jet-admin-header>
+            <div class="grid grid-cols-3 mt-6">
                 @foreach ($temp as $item)
                     <div class="flex gap-2">
                         <p>{{ $item->id }}</p>
@@ -15,54 +24,90 @@
         </x-jet-admin-card>
         <x-jet-section-border />
         <x-jet-admin-card>
-            <x-jet-admin-header>Collection Image Files <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($collection_image_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-4">
-           
+            <x-jet-admin-header>Collection Image in Files <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($collection_image_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
                 @foreach ($collection_image_files as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
                 @endforeach
             </div>
+            <x-jet-admin-header>Collection Image Path in Database <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($collection_image_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($collection_image_path as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
         </x-jet-admin-card>
         <x-jet-section-border />
         <x-jet-admin-card>
-            <x-jet-admin-header>Cover Image Files  <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($cover_image_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-4">
+            <x-jet-admin-header>Cover Image in Files <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($cover_image_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
                 @foreach ($cover_image_files as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
                 @endforeach
             </div>
+            <x-jet-admin-header>Cover Image Path in Database <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($cover_image_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($cover_image_path as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
         </x-jet-admin-card>
         <x-jet-section-border />
         <x-jet-admin-card>
-            <x-jet-admin-header>Image Back Files <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_back_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-4">
+            <x-jet-admin-header>Image Back in Files <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_back_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
                 @foreach ($image_back_files as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
                 @endforeach
             </div>
+            <x-jet-admin-header>Image Back Path in Database <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_back_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($image_back_path as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
         </x-jet-admin-card>
         <x-jet-section-border />
         <x-jet-admin-card>
-            <x-jet-admin-header>Image Front   <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_front_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-4">
+            <x-jet-admin-header>Image Front in Files<span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_front_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
                 @foreach ($image_front_files as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
                 @endforeach
             </div>
-        </x-jet-admin-card>
-        <x-jet-section-border />
-        <x-jet-admin-card>
-            <x-jet-admin-header>Mockup Image   <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($mockup_image_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-4 mt-4">
-                @foreach ($mockup_image_files as $item)
+            <x-jet-admin-header>Image Front Path in Database <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($image_front_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($image_front_path as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
@@ -71,9 +116,44 @@
         </x-jet-admin-card>
         <x-jet-section-border />
         <x-jet-admin-card>
-            <x-jet-admin-header>Profile Photos   <span class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($profile_photos_files) }}</span></x-jet-admin-header>
-            <div class="grid grid-cols-1 mt-4">
+            <x-jet-admin-header>Mockup Image in Files<span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($mockup_image_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($mockup_image_files as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
+            <x-jet-admin-header>Mockup Image Path in Database<span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($mockup_image_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-3 my-4">
+                @foreach ($mockup_image_path as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
+        </x-jet-admin-card>
+        <x-jet-section-border />
+        <x-jet-admin-card>
+            <x-jet-admin-header>Profile Photos in Files <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($profile_photos_files) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-2 my-4">
                 @foreach ($profile_photos_files as $item)
+                    <div class="flex gap-2 p-2">
+                        <p>{{ $item }}</p>
+                    </div>
+                @endforeach
+            </div>
+            <x-jet-admin-header>Profile Photos Path in Database <span
+                    class="bg-green-500 rounded-md px-2 py-0.5 ml-2">{{ count($profile_photos_path) }}</span>
+            </x-jet-admin-header>
+            <div class="grid grid-cols-2 my-4">
+                @foreach ($profile_photos_path as $item)
                     <div class="flex gap-2 p-2">
                         <p>{{ $item }}</p>
                     </div>
